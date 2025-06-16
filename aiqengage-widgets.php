f2ee4c560f0a39c5eb4982ec6c578f914bea01a7
YourlocalJay <125357195+YourlocalJay@users.noreply.github.com>
2025-06-15 20:59:57 -0700

    Add aiqengage-widgets.php to fix critical error

---
+++ aiqengage-widgets.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * AIQEngage Custom Elementor Widgets
+ *
+ * @package AIQEngage
+ */
+
+// Exit if accessed directly
+if (!defined('ABSPATH')) {
+    exit;
+}
+
+// This is a placeholder file to prevent errors
+// Proper widget implementation can be done later
+
+class AIQEngage_Elementor_Widgets {
+    public function __construct() {
+        // Basic initialization to prevent errors
+    }
+}
+
+// Initialize
+new AIQEngage_Elementor_Widgets();
